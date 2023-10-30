def3928da24faad2183ad976c4cefe682f1fa612
inhomun <inho8063@icloud.com>
2023-10-30 18:39:53 +0900

    Setting/#2 Add Network, Color file

--- CoPro/CoPro/Network/Foundation/BaseResponse.swift
+++ CoPro/CoPro/Network/Foundation/BaseResponse.swift
@@ -19,8 +19,16 @@ struct GenericResponse<T: Codable>: Codable {
     var status_message: String?
 }
 
+// - MARK: 간단한 응답에 대한 모델, statuscode만 parsing함
 struct VoidResult: Codable {
     var code: Int
     var data: Bool?
     var message: String?
+}
+
+// - MARK: 임시로 정의한 Error Template, 수정 가능
+struct ErrorDTO: Codable {
+    let statusCode: Int
+    let data: String
+    let errorMessage: String
 }
