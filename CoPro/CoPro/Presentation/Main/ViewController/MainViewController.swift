399e5e85d873892323462fb939e9519c093c01c3
inhomun <inho8063@icloud.com>
2023-11-23 23:06:39 +0900

    [Feat] #11 - Add radiobutton & add UISegmentatControl extension

---
+++ CoPro/CoPro/Presentation/Main/ViewController/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  CoPro
+//
+//  Created by 문인호 on 11/13/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
