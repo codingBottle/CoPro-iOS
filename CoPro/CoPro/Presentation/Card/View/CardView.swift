546d04e1dc7e73e67c8957bc6f94415e4e0b8982
devpark435 <devpark435@gmail.com>
2023-12-08 14:32:02 +0900

    [Feat] #12 -  Card UI

--- CoPro/CoPro/Presentation/Card/View/CardView.swift
+++ CoPro/CoPro/Presentation/Card/View/CardView.swift
@@ -12,7 +12,7 @@ import Then
 
 class CardView: BaseView {
     // UIStackView 생성
-    let stackView = UIStackView().then {
+    let buttonStackView = UIStackView().then {
         $0.axis = .horizontal  // 가로 방향으로 정렬
         $0.distribution = .fillEqually  // 모든 뷰의 크기를 동일하게 설정
         $0.spacing = 20  // 뷰 사이의 간격을 20으로 설정
@@ -59,22 +59,57 @@ class CardView: BaseView {
         $0.setImage(UIImage(systemName: "chevron.down"), for: .normal)
         $0.tintColor = UIColor(red: 0.581, green: 0.585, blue: 0.596, alpha: 1)
     }
-    
+    let slideCardView = UIView().then{
+        $0.backgroundColor = UIColor(red: 0.71, green: 0.769, blue: 0.866, alpha: 1)
+        $0.layer.cornerRadius = 30
+    }
+    let cardImage = UIImageView().then{
+        $0.backgroundColor = UIColor.white
+        $0.layer.cornerRadius = 30
+    }
+    let chatButton = UIButton().then{
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        $0.layer.cornerRadius = 15
+    }
+    let chatLabel = UILabel().then{
+        $0.textAlignment = .center
+        $0.attributedText = NSAttributedString(string: "채팅하기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.25])
+        $0.adjustsFontSizeToFitWidth = true
+        $0.minimumScaleFactor = 0.7
+    }
+    let infoView = UIView().then{
+        $0.backgroundColor = UIColor.white
+    }
+    let infoTextStackView = UIStackView()
+    let userNameLabel = UILabel()
+    let userPartLabel = UILabel()
+    let userLangLabel = UILabel()
+    let infoIconStackView = UIStackView()
+    let likeIcon = UIImage()
+//    let gitIcon = UIButton().then{
+//        $0.setImage(gitImage, for: .normal)
+//    }
+//    let gitImage = UIImage(image:Image.github_SignInButton)
+
     
     
     override func setUI() {
-        addSubview(stackView)
-        stackView.addArrangedSubview(partContainerView)
-        stackView.addArrangedSubview(langContainerView)
-        stackView.addArrangedSubview(oldContainerView)
+        addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(partContainerView)
+        buttonStackView.addArrangedSubview(langContainerView)
+        buttonStackView.addArrangedSubview(oldContainerView)
         partContainerView.addSubviews(partLabel,partButton)
         langContainerView.addSubviews(langLabel,langButton)
         oldContainerView.addSubviews(oldLabel,oldButton)
-        
+        addSubview(slideCardView)
+        slideCardView.addSubview(cardImage)
+        slideCardView.addSubview(chatButton)
+        chatButton.addSubview(chatLabel)
+        slideCardView.addSubview(infoView)
     }
     
     override func setLayout() {
-        stackView.snp.makeConstraints {
+        buttonStackView.snp.makeConstraints {
             $0.top.equalTo(self.safeAreaLayoutGuide)
             $0.centerX.equalTo(self.safeAreaLayoutGuide)
             $0.width.equalTo(self.safeAreaLayoutGuide).inset(20)
@@ -110,8 +145,32 @@ class CardView: BaseView {
             $0.trailing.equalTo(oldLabel.snp.trailing).offset(20)
             $0.centerY.equalToSuperview()
         }
-        
-        
-        
+        slideCardView.snp.makeConstraints{
+            $0.top.equalTo(buttonStackView.snp.bottom).offset(20)
+            $0.centerX.equalTo(self.safeAreaLayoutGuide)
+            $0.width.equalTo(self.safeAreaLayoutGuide).inset(20)
+            $0.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom).inset(100)
+        }
+        cardImage.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(20)
+            $0.width.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(200)
+        }
+        chatButton.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(20)
+            $0.width.equalToSuperview().inset(20)
+            $0.height.equalTo(40)
+        }
+        chatLabel.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+        infoView.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(cardImage.snp.bottom).offset(10)
+            $0.bottom.equalTo(chatButton.snp.top).offset(-10)
+            $0.width.equalToSuperview().inset(20)
+        }
     }
 }
