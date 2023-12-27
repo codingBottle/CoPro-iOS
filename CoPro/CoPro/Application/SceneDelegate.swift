eeb1e9adda4750d05874cb36baf43ea982dcc1f2
inhomun <101095144+inhomun@users.noreply.github.com>
2023-12-27 17:47:17 +0900

    Revert "[Feat] #11 - MainView UI 구현 완료"

--- CoPro/CoPro/Application/SceneDelegate.swift
+++ CoPro/CoPro/Application/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // 2.
         self.window = UIWindow(windowScene: windowScene)
     // 3.
-
         let navigationController = UINavigationController(rootViewController: LoginViewController())
         self.window?.rootViewController = navigationController
     // 4.
