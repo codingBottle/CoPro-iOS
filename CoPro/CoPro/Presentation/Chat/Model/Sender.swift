173defd90f8f24dc978c8d075cac22b91ac41ec4
ParkSY0919 <qhr498@gmail.com>
2023-12-30 15:04:38 +0900

    [Feat] #14 - add MessageModel, SenderModel

---
+++ CoPro/CoPro/Presentation/Chat/Model/Sender.swift
@@ -0,0 +1,13 @@
+//
+//  Sender.swift
+//  CoPro
+//
+//  Created by 박신영 on 12/27/23.
+//
+
+import MessageKit
+
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
+}
