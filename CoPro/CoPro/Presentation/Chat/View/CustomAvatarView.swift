0cce3ac602c439871b668386fbec8dd8ba3d41c0
ParkSY0919 <qhr498@gmail.com>
2023-12-30 14:57:29 +0900

    [Feat] #14 - AvatarView Custom

---
+++ CoPro/CoPro/Presentation/Chat/View/CustomAvatarView.swift
@@ -0,0 +1,19 @@
+//
+//  CustomAvatarView.swift
+//  ChatTest
+//
+//  Created by 박신영 on 12/17/23.
+//
+
+import UIKit
+import MessageKit
+
+class CustomAvatarView: AvatarView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        // 원하는 위치와 크기로 설정
+        let desiredFrame = CGRect(x: 20, y: 20, width: 40, height: 40)
+        self.frame = desiredFrame
+    }
+}
