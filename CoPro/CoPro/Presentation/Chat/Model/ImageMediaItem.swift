93ab6a1f41f400373ced896a3970127eb39a81ae
ParkSY0919 <qhr498@gmail.com>
2023-12-30 15:07:50 +0900

    [Feat] #14 - add ImageMediaItemModel

---
+++ CoPro/CoPro/Presentation/Chat/Model/ImageMediaItem.swift
@@ -0,0 +1,23 @@
+//
+//  ImageMediaItem.swift
+//  CoPro
+//
+//  Created by 박신영 on 12/27/23.
+//
+
+import UIKit
+import MessageKit
+
+struct ImageMediaItem: MediaItem {
+  var url: URL?
+  var image: UIImage?
+  var placeholderImage: UIImage
+  var size: CGSize
+
+  init(image: UIImage) {
+    self.image = image
+    self.size = CGSize(width: 240, height: 240)
+    self.placeholderImage = UIImage()
+  }
+}
+
