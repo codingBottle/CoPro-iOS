76efade9c4c76879ca21ea40d618c4b350d3fc5c
devpark435 <devpark435@gmail.com>
2024-01-23 21:17:36 +0900

    [Feat] #12 - update minicardUI

--- CoPro/CoPro/Presentation/Card/View/MiniCard.swift
+++ CoPro/CoPro/Presentation/Card/View/MiniCard.swift
@@ -8,97 +8,108 @@
 import UIKit
 import SnapKit
 import Then
+import Kingfisher
 
 class MiniCard: BaseView {
-    
-    
-    let shapes = UIView().then {
+    let profileArea = UIView().then{
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 15
+        $0.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.05).cgColor
+        $0.layer.shadowOpacity = 1
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowRadius = 6
+    }
+    let profile = UIImageView().then{
+        $0.layer.cornerRadius = 80/2
         $0.clipsToBounds = true
     }
+    func loadImage(url: String) {
+            guard let imageURL = URL(string: url) else { return }
+
+            profile.kf.indicatorType = .activity
+            profile.kf.setImage(
+                with: imageURL,
+                placeholder: nil,
+                options: [
+                    .transition(.fade(1.0)),
+                    .forceTransition,
+                    .cacheOriginalImage,
+                    .scaleFactor(UIScreen.main.scale), // 이미지 스케일 지정
+                    
+                ],
+                completionHandler: nil
+            )
+        }
     
-    let profile = UIImageView().then {
-        let size = CGSize(width: 80, height: 80) // 원하는 크기로 변경하세요.
-            UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
-            let context = UIGraphicsGetCurrentContext()
-            
-            context?.setFillColor(UIColor.red.cgColor) // 원하는 색상으로 변경하세요.
-            context?.fillEllipse(in: CGRect(origin: .zero, size: size))
-            let image = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            
-            $0.image = image
-            $0.contentMode = .scaleAspectFill
-            $0.clipsToBounds = true
-        
+    let userNameLabel = UILabel().then {
+        $0.textColor = UIColor(red: 0.581, green: 0.585, blue: 0.596, alpha: 1)
+        $0.textAlignment = .center
+        $0.attributedText = NSAttributedString(string: "name", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 9, weight: .regular)])
+        $0.adjustsFontSizeToFitWidth = true
+        $0.minimumScaleFactor = 0.7
     }
-    let profileLabel = UILabel().then{
-        $0.text = "Your Title"
-        $0.font = UIFont(name: "Pretendard-Regular", size: 9)
-            $0.textColor = .black
-            $0.textAlignment = .center
-    }
+    
     let lineView = UIView().then {
-        $0.backgroundColor = .lightGray // 연한 회색으로 설정
-        $0.heightAnchor.constraint(equalToConstant: 1).isActive = true // 가로줄 두께 설정
+        $0.backgroundColor = .lightGray
+        $0.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
+    
     let infoStackView = UIStackView().then {
         $0.axis = .vertical
         $0.distribution = .fill
-        $0.spacing = 8
+        $0.spacing = 1
         $0.alignment = .center
-        
     }
     
     let userPartLabel = UILabel().then {
         $0.textAlignment = .center
-        $0.attributedText = NSAttributedString(string: "Mobile", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.37])
+        $0.attributedText = NSAttributedString(string: "Mobile", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 0.84])
         $0.adjustsFontSizeToFitWidth = true
         $0.minimumScaleFactor = 0.7
     }
+    
     let userLangLabel = UILabel().then {
         $0.textAlignment = .center
-        $0.attributedText = NSAttributedString(string: "Swift / Dart", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.37])
+        $0.attributedText = NSAttributedString(string: "Swift / Dart", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.23])
         $0.adjustsFontSizeToFitWidth = true
         $0.minimumScaleFactor = 0.7
     }
+    
     let userCareerLabel = UILabel().then {
         $0.textAlignment = .center
-        $0.attributedText = NSAttributedString(string: "~6개월", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14, weight: .regular), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.21])
+        $0.attributedText = NSAttributedString(string: "~6개월", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 11, weight: .regular), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 0.95])
         $0.adjustsFontSizeToFitWidth = true
         $0.minimumScaleFactor = 0.7
     }
-    let cardbuttonStackView = UIStackView().then {
+    
+    let buttonStackView = UIStackView().then {
         $0.axis = .vertical
         $0.distribution = .fillEqually
         $0.spacing = 5
+        $0.alignment = .center
     }
+    
     let chatButton = UIButton().then {
         $0.backgroundColor =  UIColor(red: 0.71, green: 0.769, blue: 0.866, alpha: 1)
-        $0.layer.cornerRadius = 15
+        $0.layer.cornerRadius = 8
     }
+    
     let chatLabel = UILabel().then {
         $0.textAlignment = .center
-        $0.attributedText = NSAttributedString(string: "채팅하기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.25])
+        $0.attributedText = NSAttributedString(string: "채팅하기", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 8, weight: .regular), NSAttributedString.Key.foregroundColor: UIColor.black, NSAttributedString.Key.kern: 1.15])
         $0.adjustsFontSizeToFitWidth = true
         $0.minimumScaleFactor = 0.7
-    }
-    let gitImage = UIImageView(image: Image.github_SignInButton)
-    let gitButtonStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.spacing = 1
-        $0.alignment = .center
     }
     
     let gitButton: UIButton = {
         var config = UIButton.Configuration.plain()
         let gitImage = UIImage(named: "github_SignInButton")?.withRenderingMode(.alwaysTemplate)
         var container = AttributeContainer()
-        container.font = .systemFont(ofSize: 16, weight: .regular)
-        let resizedImage = gitImage?.resized(to: CGSize(width: 32, height: 32)) // 이미지 크기 조절
+        container.font = .systemFont(ofSize: 8, weight: .regular)
+        let resizedImage = gitImage?.resized(to: CGSize(width: 16, height: 16)) // 이미지 크기 조절
         
-        // 이미지와 텍스트 간 여백 설정
-        config.imagePadding = 5 // 이미지 여백
-        config.imagePlacement = .leading // 이미지 위치
+        config.imagePadding = 5
+        config.imagePlacement = .leading
         config.attributedTitle = AttributedString("Git", attributes: container)
         config.image = resizedImage
         config.baseForegroundColor = .black
@@ -106,33 +117,19 @@ class MiniCard: BaseView {
         let button = UIButton(configuration: config)
         
         button.backgroundColor =  UIColor(red: 0.71, green: 0.769, blue: 0.866, alpha: 1)
-        button.layer.cornerRadius = 15
+        button.layer.cornerRadius = 8
         
         return button
     }()
     
     override func setUI() {
-       
-        addSubview(shapes)
-        shapes.addSubviews(profile,profileLabel)
-        shapes.addSubview(lineView)
-        shapes.addSubview(infoStackView)
+        addSubviews(profileArea,profile, userNameLabel, lineView, infoStackView )
         infoStackView.addArrangedSubviews(userPartLabel,userLangLabel,userCareerLabel)
-        shapes.addSubview(cardbuttonStackView)
-        cardbuttonStackView.addArrangedSubview(gitButton)
-        cardbuttonStackView.addArrangedSubview(chatButton)
+        addSubviews(buttonStackView)
+        buttonStackView.addArrangedSubviews(chatButton, gitButton)
         chatButton.addSubview(chatLabel)
-        
-        
-       
-        
-        let layer1 = CALayer().then {
-            $0.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
-        }
-        
-        shapes.layer.addSublayer(layer1)
-        
-        shapes.layer.do {
+
+        layer.do {
             $0.cornerRadius = 15
             $0.borderWidth = 1
             $0.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
@@ -140,40 +137,51 @@ class MiniCard: BaseView {
     }
     
     override func setLayout() {
-        shapes.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        profileArea.snp.makeConstraints{
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(12)
+//            $0.centerX.equalToSuperview()
+            $0.height.equalTo(80)
+            $0.width.equalTo(80)
         }
-        profile.snp.makeConstraints{
-            $0.top.equalTo(shapes.snp.top).offset(20)
+        profile.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
             $0.centerX.equalToSuperview()
+            $0.height.equalTo(80)
+            $0.width.equalTo(80)
         }
-        profileLabel.snp.makeConstraints{
-            $0.top.equalTo(profile.snp.bottom).offset(10)
+
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profile.snp.bottom).offset(6)
             $0.centerX.equalToSuperview()
+            $0.height.greaterThanOrEqualTo(11)
         }
-        lineView.snp.makeConstraints{
-            $0.top.equalTo(profileLabel.snp.bottom).offset(10)
+        userNameLabel.setContentHuggingPriority(.required, for: .vertical)
+        userNameLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(6)
             $0.centerX.equalToSuperview()
             $0.width.equalToSuperview().dividedBy(3)
         }
+
         infoStackView.snp.makeConstraints {
-            $0.top.equalTo(lineView.snp.bottom).offset(20)
+            $0.top.equalTo(lineView.snp.bottom).offset(6)
             $0.centerX.equalToSuperview()
         }
-        cardbuttonStackView.snp.makeConstraints{
+        buttonStackView.snp.makeConstraints{
             $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(20)
+            $0.top.equalTo(infoStackView.snp.bottom).offset(8)
+            $0.bottom.equalToSuperview().offset(-12)
             $0.width.equalToSuperview().inset(20)
-            $0.height.equalTo(50)
         }
-        chatButton.snp.makeConstraints{
-            $0.centerY.equalToSuperview()
+        gitButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview()
         }
-        gitButton.snp.makeConstraints{
-            $0.centerY.equalToSuperview()
-            
+        chatButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalToSuperview()
         }
-        chatLabel.snp.makeConstraints{
+        chatLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
     }
@@ -181,43 +189,9 @@ class MiniCard: BaseView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        if let layer1 = shapes.layer.sublayers?.first {
-            layer1.bounds = shapes.bounds
-            layer1.position = shapes.center
+        if let layer1 = layer.sublayers?.first {
+            layer1.bounds = bounds
+            layer1.position = center
         }
     }
 }
-
-// 전처리
-#if DEBUG
-
-import SwiftUI
-@available(iOS 13.0, *)
-
-// UIViewControllerRepresentable을 채택
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    // update
-    // _ uiViewController: UIViewController로 지정
-    func updateUIViewController(_ uiViewController: UIViewController , context: Context) {
-        
-    }
-    // makeui
-    func makeUIViewController(context: Context) -> UIViewController {
-    // Preview를 보고자 하는 Viewcontroller 이름
-    // e.g.)
-        return CardViewController()
-    }
-}
-
-struct ViewController_Previews: PreviewProvider {
-    
-    @available(iOS 13.0, *)
-    static var previews: some View {
-        // UIViewControllerRepresentable에 지정된 이름.
-        ViewControllerRepresentable()
-
-// 테스트 해보고자 하는 기기
-            .previewDevice("iPhone 11")
-    }
-}
-#endif
