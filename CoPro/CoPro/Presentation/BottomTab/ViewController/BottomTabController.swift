4098ff40c22147a15b5ddcc80b765e1f0f44d5d6
devpark435 <devpark435@gmail.com>
2024-02-08 23:15:38 +0900

    [Feat] #31 - tabbar title 상단 노출 제거, 폰트적용

--- CoPro/CoPro/Presentation/BottomTab/ViewController/BottomTabController.swift
+++ CoPro/CoPro/Presentation/BottomTab/ViewController/BottomTabController.swift
@@ -8,60 +8,98 @@
 import UIKit
 
 class BottomTabController: UITabBarController {
+    private func addTabBarSeparator() {
+        let separator = UIView(frame: CGRect(x: 0, y: 0, width: tabBar.frame.width, height: 1))
+        separator.backgroundColor = UIColor.G3() // Set the color of the separator line
+        tabBar.addSubview(separator)
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
+        
         // 네비게이션 바를 숨깁니다.
         self.navigationController?.isNavigationBarHidden = true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        addTabBarSeparator()
         navigationController?.isNavigationBarHidden = true
-        
+        UITabBar.appearance().unselectedItemTintColor = UIColor.G3()
+        UITabBar.appearance().tintColor = UIColor.Black()
         // Do any additional setup after loading the view.
-        let homeVC = CardViewController()
-        let cardVC = CardViewController()
         let notificationVC = CardViewController() //MARK: TODO) 알림화면VC등록
-        let chatVC = CardViewController() //MARK: TODO) 알림화면VC등록
+        let cardVC = CardViewController()
+        let homeVC = CardViewController()//MARK: TODO) 홈화면VC등록
+        let chatVC = CardViewController() //MARK: TODO) 채팅화면VC등록
         let profileVC = CardViewController() //MARK: TODO) ProfileVC등록
         
         //각 tab bar의 viewcontroller 타이틀 설정
-        
-        homeVC.title = "홈"
-        cardVC.title = "카드"
         notificationVC.title = "알림"
+        cardVC.title = "카드"
+        homeVC.title = "홈"
         chatVC.title = "채팅"
         profileVC.title = "프로필"
         
-        homeVC.tabBarItem.image = UIImage.init(systemName: "house")
-        cardVC.tabBarItem.image = UIImage.init(systemName: "house")
-        notificationVC.tabBarItem.image = UIImage.init(systemName: "house")
-        chatVC.tabBarItem.image = UIImage.init(systemName: "house")
-        profileVC.tabBarItem.image = UIImage.init(systemName: "house")
+        let attributes: [NSAttributedString.Key: Any] = [
+          .font: UIFont(name: "Pretendard-Bold", size: 11)!, // 폰트 설정
+        ]
+        notificationVC.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        cardVC.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        homeVC.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        chatVC.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        profileVC.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        // 상단 탭바 타이틀 노출 제거
+        notificationVC.navigationItem.title = nil
+        cardVC.navigationItem.title = nil
+        homeVC.navigationItem.title = nil
+        chatVC.navigationItem.title = nil
+        profileVC.navigationItem.title = nil
+        // 기본상태 아이콘 설정
+        notificationVC.tabBarItem.image = UIImage.init(systemName: "bell")/*?.resizeImage(targetSize: CGSize(width: 20, height: 20))*/
+        cardVC.tabBarItem.image = UIImage.init(systemName: "rectangle.stack.badge.person.crop")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        homeVC.tabBarItem.image = UIImage.init(systemName: "house")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        chatVC.tabBarItem.image = UIImage.init(systemName: "ellipsis.message")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        profileVC.tabBarItem.image = UIImage.init(systemName: "person.crop.circle")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        //선택 상태 아이콘 설정
+        notificationVC.tabBarItem.selectedImage = UIImage(systemName: "bell.fill")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        cardVC.tabBarItem.selectedImage = UIImage(systemName: "rectangle.stack.badge.person.crop.fill")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        homeVC.tabBarItem.selectedImage = UIImage(systemName: "house.fill")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        chatVC.tabBarItem.selectedImage = UIImage(systemName: "ellipsis.message.fill")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
+        profileVC.tabBarItem.selectedImage = UIImage(systemName: "person.crop.circle.fill")?.resizeImage(targetSize: CGSize(width: 20, height: 20))
         
-        //self.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0);
-        
-        // 위에 타이틀 text를 항상 크게 보이게 설정
-        homeVC.navigationItem.largeTitleDisplayMode = .never
-        cardVC.navigationItem.largeTitleDisplayMode = .never
-        notificationVC.navigationItem.largeTitleDisplayMode = .never
-        chatVC.navigationItem.largeTitleDisplayMode = .never
-        profileVC.navigationItem.largeTitleDisplayMode = .never
+
+        // self.tabBarItem.imageInsets = UIEdgeInsets(top: 20, left: 0, bottom: -6, right: 0);
         
         // navigationController의 root view 설정
-        let navigationHome = UINavigationController(rootViewController: homeVC)
-        let navigationCard = UINavigationController(rootViewController: cardVC)
         let navigationNotification = UINavigationController(rootViewController: notificationVC)
+        let navigationCard = UINavigationController(rootViewController: cardVC)
+        let navigationHome = UINavigationController(rootViewController: homeVC)
         let navigationChat = UINavigationController(rootViewController: chatVC)
         let navigationProfile = UINavigationController(rootViewController: profileVC)
         
-        
-//        navigationHome.navigationBar.prefersLargeTitles = true
-//        navigationCard.navigationBar.prefersLargeTitles = true
-//        navigationNotification.navigationBar.prefersLargeTitles = true
-//        navigationChat.navigationBar.prefersLargeTitles = true
-//        navigationProfile.navigationBar.prefersLargeTitles = true
-        
-        setViewControllers([navigationHome, navigationCard, navigationNotification,navigationChat,navigationProfile], animated: false)
+        setViewControllers([navigationNotification, navigationCard, navigationHome,navigationChat,navigationProfile], animated: false)
+    }
+}
+extension UIImage {
+    func resizeImage(targetSize: CGSize) -> UIImage {
+        let size = self.size
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        let newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage!
     }
 }
