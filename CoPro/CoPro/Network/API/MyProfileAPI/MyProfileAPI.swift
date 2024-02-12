ff1ccc65f68ccc12e82ece5d3893abae2a87afca
ParkSY0919 <qhr498@gmail.com>
2024-02-13 03:33:30 +0900

    [Feat] #19 - add MyProfile API & Router
    edit API 3개, get API 4개

--- CoPro/CoPro/Network/API/MyProfileAPI/MyProfileAPI.swift
+++ CoPro/CoPro/Network/API/MyProfileAPI/MyProfileAPI.swift
@@ -25,4 +25,80 @@ extension MyProfileAPI {
                                 completion: completion)
         }
     }
+    
+    // MARK: - MyProfile 수정
+    
+    public func postEditMyProfile(token: String,
+                                  requestBody: EditMyProfileRequestBody,
+                                  completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.postEditMyProfile(token: token, requestBody: requestBody)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: EditMyProfileDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - Github URL 수정
+    
+    public func postEditGitHubURL(token: String,
+                                  requestBody: EditGitHubURLRequestBody,
+                                  completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.postEditGitHubURL(token: token, requestBody: requestBody)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: EditMyProfileDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - 카드뷰 타입 수정
+    
+    public func postEditCardType(token: String,
+                                 requestBody: EditCardTypeRequestBody,
+                                 completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.postEditCardType(token: token, requestBody: requestBody)).responseData { response in
+           self.disposeNetwork(response,
+                               dataModel: EditCardTypeDTO.self,
+                               completion: completion)
+       }
+   }
+    
+    // MARK: - 내가 작성한 게시물 조회
+    
+    public func getWritebyMe(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.getWritebyMe(token: token)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: WritebyMeDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - 내가 작성한 댓글 조회
+    
+    public func getMyWrittenComment(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.getMyWrittenComment(token: token)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: MyWrittenCommentDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - 스크랩 게시글 조회
+    
+    public func getScrapPost(token: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.getScrapPost(token: token)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: ScrapPostDTO.self,
+                                completion: completion)
+        }
+    }
+    
+    // MARK: - 닉네임 중복확인
+    
+    public func getNickNameDuplication(token: String, nickname: String, completion: @escaping(NetworkResult<Any>) -> Void) {
+        AFManager.request(MyProfileRouter.getNickNameDuplication(token: token, nickname: nickname)).responseData { response in
+            self.disposeNetwork(response,
+                                dataModel: getNickNameDuplicationDTO.self,
+                                completion: completion)
+        }
+    }
 }
--- CoPro/CoPro/Network/Router/MyProfileRouter/MyProfileRouter.swift
+++ CoPro/CoPro/Network/Router/MyProfileRouter/MyProfileRouter.swift
@@ -10,6 +10,13 @@ import Alamofire
 
 enum MyProfileRouter {
     case getMyPfoile(token: String)
+    case getWritebyMe(token: String)
+    case getMyWrittenComment(token: String)
+    case getScrapPost(token: String)
+    case getNickNameDuplication(token: String, nickname: String)
+    case postEditGitHubURL(token: String, requestBody: EditGitHubURLRequestBody)
+    case postEditCardType(token: String, requestBody: EditCardTypeRequestBody)
+    case postEditMyProfile(token: String, requestBody: EditMyProfileRequestBody)
 }
 
 extension MyProfileRouter: BaseTargetType {
@@ -20,13 +27,56 @@ extension MyProfileRouter: BaseTargetType {
         switch self {
         case .getMyPfoile:
             return .get
+            
+        case .getWritebyMe:
+            return .get
+            
+        case .getMyWrittenComment:
+            return .get
+            
+        case .getScrapPost:
+            return .get
+            
+        case .getNickNameDuplication:
+            return .get
+            
+        case .postEditGitHubURL:
+            return .post
+            
+        case .postEditCardType:
+            return .post
+            
+        case .postEditMyProfile:
+            return .post
         }
-        
     }
 
     var path: String {
         switch self {
         case .getMyPfoile:
+            return "/api/profile"
+            
+        case .getWritebyMe:
+            return "/api/board/write"
+        
+        case .getMyWrittenComment:
+            return "/api/comment/write"
+            
+        case .getScrapPost:
+            return "/api/scrap"
+            
+        case .getNickNameDuplication(_, _):
+            return "/api/nickname"
+//        case .getNickNameDuplication(_, let nickname):
+//            return "/api/nickname/\(nickname)"
+            
+        case .postEditGitHubURL:
+            return "/api/github-url"
+            
+        case .postEditCardType:
+            return "/api/view-type"
+            
+        case .postEditMyProfile:
             return "/api/profile"
         }
     }
@@ -35,6 +85,33 @@ extension MyProfileRouter: BaseTargetType {
         switch self {
         case .getMyPfoile:
             return .none
+            
+        case .getWritebyMe:
+            return .none
+            
+        case .getMyWrittenComment:
+            return .none
+        
+        case .getScrapPost:
+            return .none
+          
+        case .getNickNameDuplication(_, let nickname):
+            return .query(["nickname": nickname])
+
+//        case .getNickNameDuplication:
+//            return .none
+            
+//        case .getNickNameDuplication(_, let nickname):
+//            return .query(nickname)
+            
+        case .postEditGitHubURL(_, let requestBody):
+            return .body(requestBody)
+            
+        case .postEditCardType(_, let requestBody):
+            return .body(requestBody)
+            
+        case .postEditMyProfile(_, let requestBody):
+            return .body(requestBody)
         }
     }
 
@@ -42,7 +119,30 @@ extension MyProfileRouter: BaseTargetType {
         switch self{
         case .getMyPfoile(let token):
             return ["Authorization": "Bearer \(token)"]
+        
+        case .getWritebyMe(let token):
+            return ["Authorization": "Bearer \(token)"]
             
+        case .getMyWrittenComment(let token):
+            return ["Authorization": "Bearer \(token)"]
+            
+        case .getScrapPost(let token):
+            return ["Authorization": "Bearer \(token)"]
+        
+            
+//        case .getNickNameDuplication(let token):
+//            return ["Authorization": "Bearer \(token)"]
+        case .getNickNameDuplication(let token, _):
+            return ["Authorization": "Bearer \(token)"]
+            
+        case .postEditGitHubURL(let token, _):
+            return ["Authorization": "Bearer \(token)"]
+            
+        case .postEditCardType(let token, _):
+            return ["Authorization": "Bearer \(token)"]
+            
+        case .postEditMyProfile(let token, _):
+            return ["Authorization": "Bearer \(token)"]
         }
     }
 }
