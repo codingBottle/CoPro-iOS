a86f4505900b821bab261d18fadd852914728f8e
devpark435 <devpark435@gmail.com>
2024-02-15 22:03:18 +0900

    [Feat] #35 - add refresh accesstoken method

--- CoPro/CoPro/Network/API/Login/LoginAPI.swift
+++ CoPro/CoPro/Network/API/Login/LoginAPI.swift
@@ -62,5 +62,44 @@ extension LoginAPI {
             }
         
     }
+    public func refreshAccessToken(completion: @escaping (Result<LoginDTO, Error>) -> Void){
+        let keychain = KeychainSwift()
+        guard let refreshToken = keychain.get("refreshToken") else {
+            print("No refreshToken found in keychain.")
+            return
+        }
+        let headers: HTTPHeaders = [
+            "Accept": "application/json"
+        ]
+        // Body 설정
+        let parameters: Parameters = [
+            "refreshToken" : refreshToken,
+        ]
+        AF.request("https://copro.shop/api/token/access", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).validate(statusCode: 200..<300)
+            .responseDecodable(of: LoginDTO.self) { response in
+                switch response.result {
+                case .success(let loginDTO):
+                    print("재발급 로그인 성공")
+                    print("AccessToken: \(loginDTO.data.accessToken)")
+                    print("refreshToken: \(loginDTO.data.refreshToken)")
+                    let keychain = KeychainSwift()
+                    keychain.set(loginDTO.data.accessToken, forKey: "accessToken")
+                    keychain.set(loginDTO.data.refreshToken, forKey: "refreshToken")
+                    completion(.success(loginDTO))
+                case .failure(let error):
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 400...499:  // 클라이언트 에러
+                            print("로그인 실패: 클라이언트 에러")
+                        case 500...599:  // 서버 에러
+                            print("로그인 실패: 서버 에러")
+                        default:
+                            print("로그인 실패: 알 수 없는 에러")
+                        }
+                    }
+                    completion(.failure(error))
+                }
+            }
+    }
 }
 
