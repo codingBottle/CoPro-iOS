f0accdaf89806dec707e8ce8655ec713f8bb6958
devpark435 <devpark435@gmail.com>
2024-02-15 19:58:57 +0900

    [Feat] #35 - Refactor Login API

--- CoPro/CoPro/Network/API/Login/LoginAPI.swift
+++ CoPro/CoPro/Network/API/Login/LoginAPI.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import Alamofire
+import UIKit
+import KeychainSwift
 
 final class LoginAPI : BaseAPI {
     static let shared = LoginAPI()
@@ -16,7 +18,7 @@ final class LoginAPI : BaseAPI {
 extension LoginAPI {
     var baseURL: String { return Config.baseURL }
     // 로그인 API
-    public func getAccessToken(authCode: String?, provider: String, completion: @escaping (Result<LoginDTO, AFError>) -> Void) {
+    public func getAccessToken(authCode: String?, provider: String) {
         let headers: HTTPHeaders = [
             "Accept": "application/json"
         ]
@@ -24,19 +26,41 @@ extension LoginAPI {
         let parameters: Parameters = [
             "authCode" : authCode!,
         ]
-        AF.request("\(baseURL)/api/\(provider)/token", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+        AF.request("https://copro.shop/api/\(provider)/token", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).validate(statusCode: 200..<300)
             .responseDecodable(of: LoginDTO.self) { response in
                 switch response.result {
                 case .success(let loginDTO):
-                    // 성공 시의 처리
-                    completion(.success(loginDTO))
-                case .failure(let error):
-                    // 실패 시의 처리
-                    print("Decoding error: \(error)")
-                    completion(.failure(error))
+                    print("\(provider) 로그인 성공")
+                    print("AccessToken: \(loginDTO.data.accessToken)")
+                    print("refreshToken: \(loginDTO.data.refreshToken)")
+                    let keychain = KeychainSwift()
+                    keychain.set(loginDTO.data.accessToken, forKey: "accessToken")
+                    keychain.set(loginDTO.data.refreshToken, forKey: "refreshToken")
+                    DispatchQueue.main.async {
+                        let bottomTabController = BottomTabController()
+                        // 현재 활성화된 UINavigationController의 루트 뷰 컨트롤러로 설정합니다.
+                        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                           let delegate = windowScene.delegate as? SceneDelegate,
+                           let window = delegate.window {
+                            window.rootViewController = bottomTabController
+                            window.makeKeyAndVisible()
+                        }
+                    }
+                case .failure(_):
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 400...499:  // 클라이언트 에러
+                            print("로그인 실패: 클라이언트 에러")
+                        case 500...599:  // 서버 에러
+                            print("로그인 실패: 서버 에러")
+                        default:
+                            print("로그인 실패: 알 수 없는 에러")
+                        }
+                    }
+                    
                 }
             }
-
+        
     }
 }
 
