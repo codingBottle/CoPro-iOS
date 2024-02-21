5726f995752774d1fb0f614e4d55707eec0aae07
devpark435 <devpark435@gmail.com>
2024-02-22 02:18:38 +0900

    [Feat] #70 - NotificationListTableCell

--- CoPro/CoPro/Presentation/Notification/View/NotificationListCell.swift
+++ CoPro/CoPro/Presentation/Notification/View/NotificationListCell.swift
@@ -5,8 +5,67 @@
 //  Created by 박현렬 on 2/22/24.
 //
 
-import Cocoa
+import UIKit
+import SnapKit
+import Then
 
-class NotificationListCell: UIView {
+class NotificationListCell: UITableViewCell {
 
-}
+    let titleLabel = UILabel().then {
+        $0.setPretendardFont(text: "", size: 13, weight: .regular, letterSpacing: 1.25)
+        }
+    let iconImageView = UIImageView().then {
+            $0.contentMode = .scaleAspectFit
+        $0.tintColor = UIColor.P2()
+        }
+    
+        let messageLabel = UILabel().then {
+            $0.font = UIFont.systemFont(ofSize: 14)
+            $0.textColor = .gray
+            $0.numberOfLines = 2
+        }
+        
+        let timeLabel = UILabel().then {
+            $0.font = UIFont.systemFont(ofSize: 12)
+            $0.textColor = .lightGray
+            $0.textAlignment = .right
+        }
+
+        override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+            setupViews()
+            setLayoutConstraints()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        private func setupViews() {
+            contentView.addSubview(titleLabel)
+            contentView.addSubview(iconImageView)
+//            contentView.addSubview(messageLabel)
+//            contentView.addSubview(timeLabel)
+        }
+
+        private func setLayoutConstraints() {
+            iconImageView.snp.makeConstraints {
+                        $0.leading.equalToSuperview().offset(15)
+                        $0.centerY.equalToSuperview()
+                        $0.width.height.equalTo(20) // 적절한 크기로 설정
+                    }
+            titleLabel.snp.makeConstraints {
+                $0.top.equalToSuperview().offset(10)
+                $0.left.equalTo(iconImageView.snp.right).offset(10)
+                $0.centerY.equalToSuperview()
+            }
+
+//            messageLabel.snp.makeConstraints {
+//                $0.top.equalTo(titleLabel.snp.bottom).offset(5)
+//                $0.leading.trailing.equalTo(titleLabel)
+//            }
+//
+//            timeLabel.snp.makeConstraints {
+//                $0.top.trailing.equalToSuperview().offset(10)
+//            }
+        }}
