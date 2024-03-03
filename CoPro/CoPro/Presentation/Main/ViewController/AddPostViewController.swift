26b03c45035ea13dc4741b8b2a80159ce9005e0c
inhomun <inho8063@icloud.com>
2024-03-04 00:43:24 +0900

    [Feat] #113 - EditAPI

--- CoPro/CoPro/Presentation/Main/ViewController/AddPostViewController.swift
+++ CoPro/CoPro/Presentation/Main/ViewController/AddPostViewController.swift
@@ -217,7 +217,7 @@ class AddPostViewController: UIViewController, SendStringData {
                 
                 present(alertController, animated: true, completion: nil)
             }
-        else if imageUrls.count >= 5 {
+        else if imageUrls.count > 5 {
             let alertController = UIAlertController(title: nil, message: "사진은 5개 이하로만 첨부 가능합니다.", preferredStyle: .alert)
             let cancelAction = UIAlertAction(title: "확인", style: .cancel, handler: nil)
             alertController.addAction(cancelAction)
