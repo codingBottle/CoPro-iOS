84d5645493f11bafba377335692d672296600484
devpark435 <devpark435@gmail.com>
2024-03-14 19:24:41 +0900

    [Fix] #140 - Removed bottomSheet .tip size

--- CoPro/CoPro/Presentation/Main/ViewController/MainViewController.swift
+++ CoPro/CoPro/Presentation/Main/ViewController/MainViewController.swift
@@ -186,6 +186,5 @@ class MyFloatingPanelLayout: FloatingPanelLayout {
     let anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] = [
         .full: FloatingPanelLayoutAnchor(absoluteInset: 16.0, edge: .top, referenceGuide: .safeArea),
         .half: FloatingPanelLayoutAnchor(fractionalInset: 0.4, edge: .bottom, referenceGuide: .superview),
-        .tip: FloatingPanelLayoutAnchor(absoluteInset: 44.0, edge: .bottom, referenceGuide: .safeArea),
     ]
 }
